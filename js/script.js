f0adfeb01b202d682243bf56381d4d41cf4994af
Sulekha_dd <111156045+mytkoelena9@users.noreply.github.com>
2023-08-05 11:18:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,33 +3,22 @@ $(document).ready(function(){
 		arrows:true,
 		dots:true,
 		slidesToShow:3,
-		slidesToScroll: 1,
 		speed:700,
 		autoplaySpeed:800,
 		isFinite:true,
 		swipe: true,
-		asNavFor:'.sliderbig',
+		asNavFor:".sliderbig",
 		waitForAnimate:false,
-		centerMode: true,
-		focusOnSelect: true,
 	});
-
-	
 
 	$('.sliderbig').slick({
-		slidesToShow: 1,
-  		slidesToScroll: 1,
-  		arrows: false,
- 		fade: true,
-  		asNavFor: '.slider',
+		arrows:false,
+		fade:true,
+		asNavFor:".slider",
 		speed:700,
 		autoplaySpeed:800,
 		isFinite:true,
 		swipe: true,
 		waitForAnimate:false,
-		});
 	});
-
-
 });
-
